96a0515ba06caadebdd4a40f552358d609598f15
daveyx <d.rehle@gmail.com>
2017-03-14 03:10:07 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+________         __       ___      ___   _______   ___  ___   ___  ___  
+|"      "\       /""\     |"  \    /"  | /"     "| |"  \/"  | |"  \/"  |
+(.  ___  :)     /    \     \   \  //  / (: ______)  \   \  /   \   \  /  
+|: \   ) ||    /' /\  \     \\  \/. ./   \/    |     \\  \/     \\  \/   
+(| (___\ ||   //  __'  \     \.    //    // ___)_    /   /      /\.  \   
+|:       :)  /   /  \\  \     \\   /    (:      "|  /   /      /  \   \  
+(________/  (___/    \___)     \__/      \_______) |___/      |___/\___|
+------------------------------------------------------------------------
+
+#my wikipedia viewer app with react#
+https://www.freecodecamp.com/challenges/build-a-wikipedia-viewer
+
+###live demo coming soon
+http://daveyx.ga/wikipedia-viewer
+
+###view at codepen
+http://codepen.io/daveyx/full/jBwJmR/
+
+###install
+1. git clone https://github.com/daveyx/fcc-wikipedia-viewer.git
+2. cd fcc-weather-app
+3. npm install
+4. ./node_modules/webpack/bin/webpack.js -d --watch
+5. call src/client/index.html in your browser
+6. enjoy...
---
+++ src/client/app/index.jsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import {render} from 'react-dom';
+
+/*
+  RandomLink Component
+*/
+class RandomLink extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <div>
+            <a className="random-link" href="https://en.wikipedia.org/wiki/Special:Random" target="_blank">Click here for a random article</a>
+            </div>;
+  }
+}
+
+/*
+  SearchArea Component
+*/
+class SearchArea extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue: ''
+    };
+  }
+  onClick() {
+    console.log("inputvalue: " + this.state.inputValue);
+  }
+  updateInputValue(evt) {
+    this.setState({
+      inputValue: evt.target.value
+    });
+  }
+  render() {
+    return <div className="search-area">
+            <div>Search for an article:</div>
+            <div>
+              <input type="text" name="search" value={this.state.inputValue}  onChange={(e) => {this.updateInputValue(e)}}/>
+              <button onClick={(e) => {this.onClick(e)}}>search</button>
+            </div>
+            <ResultArea />
+           </div>;
+  }
+}
+
+/*
+  ResultArea Component
+*/
+class ResultArea extends React.Component {
+  render() {
+    return <div className="result-area">
+            <div>fitzefatze</div>
+           </div>;
+  }
+}
+
+/*
+  Application Component
+*/
+class Application extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+  render() {
+    return <div>
+            <RandomLink />
+            <SearchArea />
+          </div>;
+  }
+}
+render(<Application />, document.getElementById('container'));

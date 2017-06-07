3a400d6a6ec8ff148633292efcfd8bf8f598cf7b
daveyx <d.rehle@gmail.com>
2017-06-07 09:41:38 +0200

    copy js from codepen

---
+++ public/03_random_quote_machine/functions.js
@@ -0,0 +1,46 @@
+var currentQuote;
+
+function printQuote(quoteJson) {
+  var json = JSON.parse(quoteJson);
+  $(".quote-box").animate({
+          opacity: 0
+        }, 500,
+        function() {
+          $(this).animate({
+            opacity: 1
+          }, 500);
+          $('.quote-box').html("\"" + json.quote + "\"");
+        });
+
+  $(".author-box").animate({
+    opacity: 0
+  }, 500,
+                           function() {
+    $(this).animate({
+      opacity: 1
+    }, 500);
+    $('.author-box').html("by " + json.author);
+  });
+};
+
+function getQuote() {
+  $.ajax({
+    headers: {
+      "X-Mashape-Key": "OivH71yd3tmshl9YKzFH7BTzBVRQp1RaKLajsnafgL2aPsfP9V",
+      Accept: "application/json",
+      "Content-Type": "application/x-www-form-urlencoded"
+    },
+    url: 'https://andruxnet-random-famous-quotes.p.mashape.com/cat=',
+    success: function(response) {
+      console.log("---------------response------------------")
+      console.log(response)
+      currentQuote = JSON.parse(response);
+      printQuote(response);
+      $('#tweet-button').attr('href', 'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + currentQuote.quote + '" ' + currentQuote.author));
+     }});
+};
+
+$(document).ready(function() {
+  getQuote();
+  $('#quote-button').on('click', getQuote);
+});

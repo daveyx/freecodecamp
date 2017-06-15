22228ae65d034f1cb6e0a0b4572a5e7d351740f2
daveyx <d.rehle@gmail.com>
2017-06-15 12:40:45 +0200

    moved output up

---
+++ src/frontend/07_calculator/webpack.config.js
@@ -0,0 +1,40 @@
+var path = require('path');
+var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+var BUILD_DIR = path.resolve(__dirname, '../../../public/frontend/07_calculator');
+
+module.exports = {
+  devServer: {
+    inline: true,
+    contentBase: BUILD_DIR,
+    port: 3000
+  },
+  devtool: 'cheap-module-eval-source-map',
+  entry: './dev/js/index.js',
+  output: {
+    path: BUILD_DIR,
+    filename: 'js/bundle.min.js',
+    publicPath: '/'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: ['babel-loader'],
+        exclude: /node_modules/
+      }, {
+        test: /\.css/,
+        loader: ExtractTextPlugin.extract("css-loader")
+      }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new ExtractTextPlugin("css/styles.css"),
+    new HtmlWebpackPlugin({
+        template: 'dev/index.html'
+    })
+  ]
+};

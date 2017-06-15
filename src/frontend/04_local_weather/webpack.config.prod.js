09105c77c0ce997da7443209f01c234fc5df5617
daveyx <d.rehle@gmail.com>
2017-06-15 10:11:43 +0200

    config for prod

--- src/frontend/04_local_weather/webpack.config.prod.js
+++ src/frontend/04_local_weather/webpack.config.prod.js
@@ -1,16 +1,36 @@
 var webpack = require('webpack');
 var path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
-var BUILD_DIR = path.resolve(__dirname, 'src/client/public');
+var BUILD_DIR = path.resolve(__dirname, '../../../public/frontend/04_local_weather');
 var APP_DIR = path.resolve(__dirname, 'src/client/app');
 
 var config = {
   entry: APP_DIR + '/index.jsx',
   output: {
     path: BUILD_DIR,
-    filename: 'bundle.js'
+    filename: 'js/bundle.[hash].js',
+    publicPath: '/freecodecamp/frontend/04_local_weather'
+  },
+  module : {
+    loaders : [
+      {
+        test : /\.jsx?/,
+        include : APP_DIR,
+        loader : 'babel-loader'
+      }, {
+        test: /\.css/,
+        loader: ExtractTextPlugin.extract("css-loader")
+      }
+    ]
   },
   plugins: [
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new ExtractTextPlugin("css/styles.css"),
+    new HtmlWebpackPlugin({
+        template: 'src/client/index.html'
+    }),
     new webpack.DefinePlugin({ // <-- key to reducing React's size
       'process.env': {
         'NODE_ENV': JSON.stringify('production')
@@ -18,16 +38,7 @@ var config = {
     }),
     new webpack.optimize.UglifyJsPlugin(), //minify everything
     new webpack.optimize.AggressiveMergingPlugin()//Merge chunks
-  ],
-  module : {
-    loaders : [
-      {
-        test : /\.jsx?/,
-        include : APP_DIR,
-        loader : 'babel-loader'
-      }
-    ]
-  }
+  ]
 };
 
 module.exports = config;

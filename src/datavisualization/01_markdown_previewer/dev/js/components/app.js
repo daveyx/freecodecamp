ce51630e2bb39a37d20ee108481bbb0d0c4d52ac
daveyx <d.rehle@gmail.com>
2017-06-21 09:01:21 +0200

    header

--- src/datavisualization/01_markdown_previewer/dev/js/components/app.js
+++ src/datavisualization/01_markdown_previewer/dev/js/components/app.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import Input from './input';
 import Output from './output';
+import '../../css/app.css';
 
 export default class App extends Component {
   constructor(props) {
@@ -10,8 +11,14 @@ export default class App extends Component {
   render() {
     return(
       <div className="wrapper">
-        <small>Free Code Camp</small>
-        <h1>Markdown Previewer</h1>
+        <header>
+          <small>Free Code Camp</small>
+          <h1>Markdown Previewer</h1>
+          <div>by <a href="https://www.daveyx.ga" target="_blank">daveyx</a></div>
+        </header>
+        <main>
+
+        </main>
       </div>
     );
   }

c8ba0b723c4d17f659a26adcc96208f5952e9b04
daveyx <d.rehle@gmail.com>
2017-06-22 07:48:05 +0200

    added babel polyfill

--- src/frontend/10_simongame/webpack.prod.config.js
+++ src/frontend/10_simongame/webpack.prod.config.js
@@ -6,7 +6,7 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 var BUILD_DIR = path.resolve(__dirname, '../../../public/frontend/10_simongame');
 
 module.exports = {
-  entry: './dev/js/index.js',
+  entry: ['babel-polyfill', './dev/js/index.js'],
   output: {
     path: BUILD_DIR,
     filename: 'js/bundle.[hash].js',
